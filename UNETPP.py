a2afe62ddcf32c07959554d5eecd4afbc55d7ffe
pizadi <parham.avia@gmail.com>
2022-12-29 18:12:54 +0330

    Final Bug fixes on UNet++

--- UNETPP.py
+++ UNETPP.py
@@ -22,40 +22,40 @@ class UNetPP(nn.Module):
 
         self.input = nn.Conv2d(3, self.scale, (1, 1),padding="same")
 
-        self.b0_0 = convblock(self.scale)
-        self.b0_1 = convblock(self.scale)
+        self.b0_0 = self.convblock(self.scale)
+        self.b0_1 = self.convblock(self.scale)
         self.up0_1 = nn.ConvTranspose2d(self.scale, self.scale, 2, stride=(2, 2))
-        self.b0_2 = convblock(self.scale)
+        self.b0_2 = self.convblock(self.scale)
         self.up0_2 = nn.ConvTranspose2d(self.scale, self.scale, 2, stride=(2, 2))
-        self.b0_3 = convblock(self.scale)
-        self.up0_2 = nn.ConvTranspose2d(self.scale, self.scale, 2, stride=(2, 2))
-        self.b0_4 = convblock(self.scale)
-        self.up0_2 = nn.ConvTranspose2d(self.scale, self.scale, 2, stride=(2, 2))
+        self.b0_3 = self.convblock(self.scale)
+        self.up0_3 = nn.ConvTranspose2d(self.scale, self.scale, 2, stride=(2, 2))
+        self.b0_4 = self.convblock(self.scale)
+        self.up0_4 = nn.ConvTranspose2d(self.scale, self.scale, 2, stride=(2, 2))
 
-        self.b1_0 = convblock(self.scale * 2)
-        self.b1_1 = convblock(self.scale * 2)
+        self.b1_0 = self.convblock(self.scale * 2)
+        self.b1_1 = self.convblock(self.scale * 2)
         self.up1_1 = nn.ConvTranspose2d(self.scale * 2, self.scale * 2, 2, stride=(2, 2))
-        self.b1_2 = convblock(self.scale * 2)
+        self.b1_2 = self.convblock(self.scale * 2)
         self.up1_2 = nn.ConvTranspose2d(self.scale * 2, self.scale * 2, 2, stride=(2, 2))
-        self.b1_3 = convblock(self.scale * 2)
+        self.b1_3 = self.convblock(self.scale * 2)
         self.up1_3 = nn.ConvTranspose2d(self.scale * 2, self.scale * 2, 2, stride=(2, 2))
 
-        self.b2_0 = convblock(self.scale * 5)
-        self.b2_1 = convblock(self.scale * 5)
+        self.b2_0 = self.convblock(self.scale * 5)
+        self.b2_1 = self.convblock(self.scale * 5)
         self.up2_1 = nn.ConvTranspose2d(self.scale * 5, self.scale * 5, 2, stride=(2, 2))
-        self.b2_2 = convblock(self.scale * 5)
+        self.b2_2 = self.convblock(self.scale * 5)
         self.up2_2 = nn.ConvTranspose2d(self.scale * 5, self.scale * 5, 2, stride=(2, 2))
 
-        self.b3_0 = convblock(self.scale * 13)
-        self.b3_1 = convblock(self.scale * 13)
+        self.b3_0 = self.convblock(self.scale * 13)
+        self.b3_1 = self.convblock(self.scale * 13)
         self.up3_1 = nn.ConvTranspose2d(self.scale * 13, self.scale * 13, 2, stride=(2, 2))
 
-        self.b4_0 = convblock(self.scale * 34)
+        self.b4_0 = self.convblock(self.scale * 34)
 
-        self.out1 = nn.Conv2d(scale*2, 1, (1, 1))
-        self.out2 = nn.Conv2d(scale*5, 1, (1, 1))
-        self.out3 = nn.Conv2d(scale*13, 1, (1, 1))
-        self.out4 = nn.Conv2d(scale*34, 1, (1, 1))
+        self.out1 = nn.Conv2d(self.scale*2, 1, (1, 1))
+        self.out2 = nn.Conv2d(self.scale*5, 1, (1, 1))
+        self.out3 = nn.Conv2d(self.scale*13, 1, (1, 1))
+        self.out4 = nn.Conv2d(self.scale*34, 1, (1, 1))
 
         if (loss_fn is None):
             self.loss_fn = nn.BCELoss()
@@ -87,10 +87,10 @@ class UNetPP(nn.Module):
         X = self.input(X)
 
         x0_0 = self.b0_0(X)
-        x0_1 = self.b0_1(F.max_pool2d(x0, (2, 2), (2, 2)))
-        x0_2 = self.b0_2(F.max_pool2d(x1, (2, 2), (2, 2)))
-        x0_3 = self.b0_3(F.max_pool2d(x2 (2, 2), (2, 2)))
-        x0_4 = self.b0_4(F.max_pool2d(x3, (2, 2), (2, 2)))
+        x0_1 = self.b0_1(F.max_pool2d(x0_0, (2, 2), (2, 2)))
+        x0_2 = self.b0_2(F.max_pool2d(x0_1, (2, 2), (2, 2)))
+        x0_3 = self.b0_3(F.max_pool2d(x0_2, (2, 2), (2, 2)))
+        x0_4 = self.b0_4(F.max_pool2d(x0_3, (2, 2), (2, 2)))
 
         x1_0 = self.b1_0(torch.cat([x0_0, self.up0_1(x0_1)], dim=1))
         x1_1 = self.b1_1(torch.cat([x0_1, self.up0_2(x0_2)], dim=1))
@@ -106,10 +106,10 @@ class UNetPP(nn.Module):
 
         x4_0 = self.b4_0(torch.cat([x0_0, x1_0, x2_0,  x3_0, self.up3_1(x3_1)], dim=1))
 
-        y1 = torch.sigmoid(self.out1(x0_1))
-        y2 = torch.sigmoid(self.out2(x0_2))
-        y3 = torch.sigmoid(self.out3(x0_3))
-        y4 = torch.sigmoid(self.out4(x0_4))
+        y1 = torch.sigmoid(self.out1(x1_0))
+        y2 = torch.sigmoid(self.out2(x2_0))
+        y3 = torch.sigmoid(self.out3(x3_0))
+        y4 = torch.sigmoid(self.out4(x4_0))
 
         return y1, y2, y3, y4
 
